cfc301606f7b8eb38d6bd070766962bae4e5aeef
joaquimmnetto <joaquimmnetto@gmail.com>
2020-02-12 00:48:07 -0300

    a

--- so_install.sh
+++ so_install.sh
@@ -9,8 +9,8 @@ mkfs.ext4 /dev/sda2
 mkfs.fat -F 32 /dev/sda1
 
 echo "Mounting partitions"
-mkdir /mnt/boot
 mount /dev/sda2 /mnt
+mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 
 echo "Installing kernel + basics"
@@ -22,7 +22,8 @@ genfstab /mnt >> /mnt/etc/fstab
 
 PART_UUID=$(blkid -s PARTUUID -o value /dev/sda2)
 echo "Adding EFI boot entry ($PART_UUID)"
-efibootmgr --create --disk /dev/sda --part 1 --label "Arch Linux" --loader /vmlinuz-linux --unicode "root=PARTUUID=$PART_UUID rw initrd=\initramfs-linux.img" --verbose
+efibootmgr --create --disk /dev/sda --part 1 --label "Arch Linux" --loader /vmlinuz-linux 
+--unicode "'root=PARTUUID=$PART_UUID rw initrd=\initramfs-linux.img'" --verbose
 
 echo "Preparing basic install"
 chmod +x basic_install.sh
