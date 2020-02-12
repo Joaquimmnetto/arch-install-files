857da5ad06b718715095508ee64094852192d3f7
joaquimmnetto <joaquimmnetto@gmail.com>
2020-02-11 23:24:28 -0300

    cp basic_install

--- so_install.sh
+++ so_install.sh
@@ -22,3 +22,10 @@ echo "fstab generator"
 echo "Adding EFI boot entry ($PART_UUID)"
 PART_UUID=$(blkid -s UUID -o value /dev/sda2)
 efibootmgr --create --disk /dev/sda --part 1 --label "Arch Linux" --loader /vmlinuz-linux --unicode "root=PARTUUID=$PART_UUID rw initrd=\initramfs-linux.img" --verbose
+
+echo "Preparing basic install"
+chmod +x basic_install.sh
+cp basic_install.sh /mnt/home
+
+echo "Rooting into new SO"
+arch-chroot /mnt
