372cc2c4ab17c8588ad22de4702e247b50a12e53
joaquimmnetto <joaquimmnetto@gmail.com>
2020-02-11 23:15:42 -0300

    fix in part_uuid assignment

--- efiboot.sh
+++ efiboot.sh
@@ -2,26 +2,25 @@
 timedatectl set-ntp true
 
 echo "Partition config from sda-part.dump"
-sfdisk /dev/sda < sda-part.dump
+#sfdisk /dev/sda < sda-part.dump
 
 echo "Formatting partitions"
-mkfs.ext4 /dev/sda2
-mkfs.fat -F 32 /dev/sda1
+#mkfs.ext4 /dev/sda2
+#mkfs.fat -F 32 /dev/sda1
 
 echo "Mounting partitions"
-mkdir /mnt/boot
-mount /dev/sda2 /mnt
-mount /dev/sda1 /mnt/boot
+#mkdir /mnt/boot
+#mount /dev/sda2 /mnt
+#mount /dev/sda1 /mnt/boot
 
 echo "Installing kernel + basics"
-pacstrap /mnt base linux linux-firmware man-db man-pages texinfo efibootmgr vim
+#pacstrap /mnt base linux linux-firmware man-db man-pages texinfo efibootmgr vim
 
 echo "fstab generator"
-genfstab /mnt >> /mnt/etc/fstab
+#genfstab /mnt >> /mnt/etc/fstab
 
-
-set PART_UUID=$(eval "blkid -s UUID -o value /dev/sda2")
 echo "Adding EFI boot entry ($PART_UUID)"
+PART_UUID=$(blkid -s UUID -o value /dev/sda2)
 efibootmgr --create --disk /dev/sda --part 1 --label "Arch Linux" --loader /vmlinuz-linux --unicode "root=PARTUUID=$PART_UUID rw initrd=\initramfs-linux.img" --verbose
 
 echo "Changing root to newly installed system"
