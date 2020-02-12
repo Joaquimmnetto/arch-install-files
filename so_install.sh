96b7e345d6085624e23a8281bdf9747ab33ad372
joaquimmnetto <joaquimmnetto@gmail.com>
2020-02-12 00:10:56 -0300

    Changed UUID to PARTUUID

--- so_install.sh
+++ so_install.sh
@@ -14,13 +14,14 @@ mount /dev/sda2 /mnt
 mount /dev/sda1 /mnt/boot
 
 echo "Installing kernel + basics"
-pacstrap /mnt base linux linux-firmware man-db man-pages texinfo efibootmgr vim
+pacstrap /mnt base linux linux-firmware man-db man-pages texinfo vim
 
 echo "fstab generator"
 genfstab /mnt >> /mnt/etc/fstab
 
+
+PART_UUID=$(blkid -s PARTUUID -o value /dev/sda2)
 echo "Adding EFI boot entry ($PART_UUID)"
-PART_UUID=$(blkid -s UUID -o value /dev/sda2)
 efibootmgr --create --disk /dev/sda --part 1 --label "Arch Linux" --loader /vmlinuz-linux --unicode "root=PARTUUID=$PART_UUID rw initrd=\initramfs-linux.img" --verbose
 
 echo "Preparing basic install"
